d8600e07fbdbcbc91cd0f618e38c98da0865991d
Adriano Rutz <adriano.rutz@hotmail.com>
2024-05-09 10:48:47 +0200

    up

--- src/smartclass/chem/similarity/__init__.py
+++ src/smartclass/chem/similarity/__init__.py
@@ -1,7 +1,9 @@
 """Smartclass classifies structures using SMARTS.chem.similarity."""
 
-# from smartclass.chem.similarity.calculate_myopic_mces import calculate_myopic_mces
+from smartclass.chem.similarity.calculate_mcs import calculate_mcs  # noqa:F401
+
+# from smartclass.chem.similarity.calculate_myopic_mces import calculate_myopic_mces  # noqa:F401
 # from smartclass.chem.similarity.calculate_structural_similarity import (
-#     calculate_structural_similarity,
+#     calculate_structural_similarity,  # noqa:F401
 # )
-# from smartclass.chem.similarity.measure_mhfp import measure_mhfp
+# from smartclass.chem.similarity.measure_mhfp import measure_mhfp  # noqa:F401
--- src/smartclass/io/load_smiles.py
+++ src/smartclass/io/load_smiles.py
@@ -2,8 +2,6 @@
 
 from __future__ import annotations
 
-import logging
-
 import polars
 
 __all__ = [
